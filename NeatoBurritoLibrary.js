027368f9e3e4828753e4cc073210ed5a5eb78769
SSDesigns <boardssw@gmail.com>
2019-08-04 23:30:27 -0700

    Update NeatoBurritoLibrary.js

--- NeatoBurritoLibrary.js
+++ NeatoBurritoLibrary.js
@@ -29,9 +29,9 @@ var NeatoLib = {
 			const updateLibrary = () => {
 				const vm = require("vm");
 
-				fetch("https://ssdesigns.github.io/KHBThemes2/PluginLibary.js", { cache: "no-cache" }).then(r => r.text()).then(data => {
+				fetch("https://ssdesigns.github.io/KHBThemes2/NeatoBurritoLibrary.js", { cache: "no-cache" }).then(r => r.text()).then(data => {
 					let lib = new vm.Script(data, {
-						filename: "PluginLibary.js",
+						filename: "NeatoBurritoLibrary.js",
 						displayErrors: true
 					});
 
@@ -57,7 +57,7 @@ var NeatoLib = {
 	forceLibUpdate: function() {
 		const vm = require("vm");
 
-		fetch("https://raw.githubusercontent.com/Metalloriff/BetterDiscordPlugins/master/Lib/NeatoBurritoLibrary.js", { cache: "no-cache" }).then(r => r.text()).then(data => {
+		fetch("https://raw.githubusercontent.com/SSDesigns/KHBThemes2/master/NeatoBurritoLibrary.js", { cache: "no-cache" }).then(r => r.text()).then(data => {
 			const lib = new vm.Script(data, { filename: "NeatoBurritoLibrary.js", displayErrors: true });
 
 			new Promise(e => e(lib.runInThisContext())).then(() => NeatoLib.showToast("Lib updated!", "success"));
